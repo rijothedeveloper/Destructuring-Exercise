036d6e857b1f1cb67b07d51580e3774628b91e63
Rijo George <rijothedeveloper@gmail.com>
2021-11-12 11:20:15 -0800

    done

--- script.js
+++ script.js
@@ -82,6 +82,8 @@ let planetFacts = {
 
         /* Write an ES2015 Version */
 
-        let arr = [1,2];
+        const arr = [1,2];
         [arr[0], arr[1]] = [arr[1], arr[0]];
         console.log(arr);
+
+        const raceResults = ([first, second, third, ...rest]) => ({first,second,third, rest})
